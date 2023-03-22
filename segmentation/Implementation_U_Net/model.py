5414673c14ca5ee01ea4c4f27644eac37cd64fb5
baemacs <mackristan@gmail.com>
2023-03-22 19:07:56 +0800

    Deeplab implementation for masking segmentation

---
+++ segmentation/Implementation_U_Net/model.py
@@ -0,0 +1,269 @@
+
+import os
+import cv2
+import numpy as np
+from glob import glob
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.models import Model
+from tensorflow.keras.applications import ResNet50
+from keras.layers import Concatenate,AveragePooling2D,Reshape,Dense,GlobalAveragePooling2D,Activation, Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
+
+def SqueezeAndExcite(inputs, ratio=8):
+    init = inputs
+    filters = init.shape[-1]
+    se_shape = (1, 1, filters)
+
+    se = GlobalAveragePooling2D()(init)
+    se = Reshape(se_shape)(se)
+    se = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
+    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
+    x = init * se
+    return x
+
+def ASPP(inputs):
+    """ Image Pooling """
+    shape = inputs.shape
+    y1 = AveragePooling2D(pool_size=(shape[1], shape[2]))(inputs)
+    y1 = Conv2D(256, 1, padding="same", use_bias=False)(y1)
+    y1 = BatchNormalization()(y1)
+    y1 = Activation("relu")(y1)
+    y1 = UpSampling2D((shape[1], shape[2]), interpolation="bilinear")(y1)
+
+    """ 1x1 conv """
+    y2 = Conv2D(256, 1, padding="same", use_bias=False)(inputs)
+    y2 = BatchNormalization()(y2)
+    y2 = Activation("relu")(y2)
+
+    """ 3x3 conv rate=6 """
+    y3 = Conv2D(256, 3, padding="same", use_bias=False, dilation_rate=6)(inputs)
+    y3 = BatchNormalization()(y3)
+    y3 = Activation("relu")(y3)
+
+    """ 3x3 conv rate=12 """
+    y4 = Conv2D(256, 3, padding="same", use_bias=False, dilation_rate=12)(inputs)
+    y4 = BatchNormalization()(y4)
+    y4 = Activation("relu")(y4)
+
+    """ 3x3 conv rate=18 """
+    y5 = Conv2D(256, 3, padding="same", use_bias=False, dilation_rate=18)(inputs)
+    y5 = BatchNormalization()(y5)
+    y5 = Activation("relu")(y5)
+
+    y = Concatenate()([y1, y2, y3, y4, y5])
+    y = Conv2D(256, 1, padding="same", use_bias=False)(y)
+    y = BatchNormalization()(y)
+    y = Activation("relu")(y)
+
+    return y
+
+def deeplabv3_plus(shape):
+    """ Input """
+    inputs = Input(shape)
+
+    """ Encoder """
+    encoder = ResNet50(weights="imagenet", include_top=False, input_tensor=inputs)
+
+    image_features = encoder.get_layer("conv4_block6_out").output
+    x_a = ASPP(image_features)
+    x_a = UpSampling2D((4, 4), interpolation="bilinear")(x_a)
+
+    x_b = encoder.get_layer("conv2_block2_out").output
+    x_b = Conv2D(filters=48, kernel_size=1, padding='same', use_bias=False)(x_b)
+    x_b = BatchNormalization()(x_b)
+    x_b = Activation('relu')(x_b)
+
+    x = Concatenate()([x_a, x_b])
+    x = SqueezeAndExcite(x)
+
+    x = Conv2D(filters=256, kernel_size=3, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters=256, kernel_size=3, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SqueezeAndExcite(x)
+
+    x = UpSampling2D((4, 4), interpolation="bilinear")(x)
+    x = Conv2D(1, 1)(x)
+    x = Activation("sigmoid")(x)
+
+    model = Model(inputs, x)
+    return model
+
+
+def convolution_block(
+    block_input,
+    num_filters=256,
+    kernel_size=3,
+    dilation_rate=1,
+    padding="same",
+    use_bias=False,
+):
+    x = layers.Conv2D(
+        num_filters,
+        kernel_size=kernel_size,
+        dilation_rate=dilation_rate,
+        padding="same",
+        use_bias=use_bias,
+        kernel_initializer=keras.initializers.HeNormal(),
+    )(block_input)
+    x = layers.BatchNormalization()(x)
+    return tf.nn.relu(x)
+
+
+def DilatedSpatialPyramidPooling(dspp_input):
+    dims = dspp_input.shape
+    x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(dspp_input)
+    x = convolution_block(x, kernel_size=1, use_bias=True)
+    out_pool = layers.UpSampling2D(
+        size=(dims[-3] // x.shape[1], dims[-2] // x.shape[2]), interpolation="bilinear",
+    )(x)
+
+    out_1 = convolution_block(dspp_input, kernel_size=1, dilation_rate=1)
+    out_6 = convolution_block(dspp_input, kernel_size=3, dilation_rate=6)
+    out_12 = convolution_block(dspp_input, kernel_size=3, dilation_rate=12)
+    out_18 = convolution_block(dspp_input, kernel_size=3, dilation_rate=18)
+
+    x = layers.Concatenate(axis=-1)([out_pool, out_1, out_6, out_12, out_18])
+    output = convolution_block(x, kernel_size=1)
+    return output
+
+def DeeplabV3Plus(image_size):
+    model_input = keras.Input(shape=(image_size, image_size, 3))
+    resnet50 = keras.applications.ResNet50(
+        weights="imagenet", include_top=False, input_tensor=model_input
+    )
+    x = resnet50.get_layer("conv4_block6_2_relu").output
+    x = DilatedSpatialPyramidPooling(x)
+
+    input_a = layers.UpSampling2D(
+        size=(image_size // 4 // x.shape[1], image_size // 4 // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+    input_b = resnet50.get_layer("conv2_block3_2_relu").output
+    input_b = convolution_block(input_b, num_filters=48, kernel_size=1)
+
+    x = layers.Concatenate(axis=-1)([input_a, input_b])
+    x = convolution_block(x)
+    x = convolution_block(x)
+    x = layers.UpSampling2D(
+        size=(image_size // x.shape[1], image_size // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+    x = Conv2D(1, 1)(x)
+    model_output = Activation("sigmoid")(x)
+    return keras.Model(inputs=model_input, outputs=model_output)
+
+
+def multi_unet_model(n_classes=1, IMG_HEIGHT=512, IMG_WIDTH=512, IMG_CHANNELS=1):
+    #Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    #s = Lambda(lambda x: x / 255)(inputs)   #No need for this if we normalize our inputs beforehand
+    s = inputs
+
+    #Contraction path
+    c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.2)(c1)  # Original 0.1
+    c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    
+    c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.2)(c2)  # Original 0.1
+    c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+     
+    c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+     
+    c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+     
+    c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    #Expansive path 
+    u6 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+     
+    u7 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+     
+    u8 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.2)(c8)  # Original 0.1
+    c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+     
+    u9 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.2)(c9)  # Original 0.1
+    c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+     
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    #NOTE: Compile the model in the main program to make it easy to test with various loss functions
+    #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
+    #model.summary()
+    
+    return model
+
+def down_block(x, filters, kernel_size=(3, 3), padding="same", strides=1):
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    p = keras.layers.MaxPool2D((2, 2), (2, 2))(c)
+    return c, p
+
+def up_block(x, skip, filters, kernel_size=(3, 3), padding="same", strides=1):
+    us = keras.layers.UpSampling2D((2, 2))(x)
+    concat = keras.layers.Concatenate()([us, skip])
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(concat)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    return c
+
+def bottleneck(x, filters, kernel_size=(3, 3), padding="same", strides=1):
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    return c
+
+def Unet():
+    f = [32, 64, 128, 256, 512]
+    inputs = keras.layers.Input((512, 512, 1))
+    
+    p0 = inputs
+    c1, p1 = down_block(p0, f[0]) #128 -> 64
+    c2, p2 = down_block(p1, f[1]) #64 -> 32
+    c3, p3 = down_block(p2, f[2]) #32 -> 16
+    c4, p4 = down_block(p3, f[3]) #16->8
+    
+    bn = bottleneck(p4, f[4])
+    
+    u1 = up_block(bn, c4, f[3]) #8 -> 16
+    u2 = up_block(u1, c3, f[2]) #16 -> 32
+    u3 = up_block(u2, c2, f[1]) #32 -> 64
+    u4 = up_block(u3, c1, f[0]) #64 -> 128
+    
+    outputs = keras.layers.Conv2D(1, (1, 1), padding="same", activation="sigmoid")(u4)
+    model = keras.models.Model(inputs, outputs)
+    return model 
+
